976d3e5492edd015b7e619b459304093fe6604e2
ugurcandemiir <ugurcandemiir@gmail.com>
2021-02-17 12:07:13 -0500

    Added Button and picture of my cat named Whiskey

--- proj1-helloworld/proj1-helloworld/ViewController.swift
+++ proj1-helloworld/proj1-helloworld/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func pushButton(_ sender: UIButton) {
+        myLabel.text = "You Clicked The Button!"
 
+    }
+    
 }
 
