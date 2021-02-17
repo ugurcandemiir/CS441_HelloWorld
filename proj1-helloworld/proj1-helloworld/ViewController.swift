3c65d36cb9f28720feecb2f83e29242f69d09629
ugurcandemiir <ugurcandemiir@gmail.com>
2021-02-17 10:27:10 -0500

    Created Project on Xcode and added files into Git.

---
+++ proj1-helloworld/proj1-helloworld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  proj1-helloworld
+//
+//  Created by Ugurcan Demir on 2/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
